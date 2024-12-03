bb8ea9083b2826544688bbc65a6e33331c45c779
fhoxha5 <149434001+fhoxha5@users.noreply.github.com>
2024-12-03 16:12:56 +0100

    added details to about page

---
+++ rrethnesh.js
@@ -0,0 +1,21 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    document.getElementById('slider').style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
