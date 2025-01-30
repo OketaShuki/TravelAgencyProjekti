5bfb1f78f42a2111d0effa06c9d695edfa6da0a3
OketaShuki <os64936@ubt-uni.net>
2025-01-30 14:26:33 -0800

    Shtova file-in ShtoFoto.php ku eshte krijuar nje form e cila mundeson shtimin e nje fotoje titulli dhe description ne menyre qe te marrim feedback nga klientet tane

---
+++ shtoFoto.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="shtoFoto.css">
+    <title>Shto nje Feedback</title>
+</head>
+<body>
+
+    <h2>Shto një review nga pushimet tuaja me agjencinë tonë:</h2>
+    
+    <form action="shtimiIFotos.php" method="post">
+        <label for="title">Title:</label><br>
+        <input type="text" id="title" name="title" required><br><br>
+
+        <label for="description">Description:</label><br>
+        <textarea id="description" name="description" rows="4" required></textarea><br><br>
+
+        <label for="image">Select an Image:</label><br>
+        <select id="image" name="image">
+            <?php
+            $image_folder = "fototprojekt/"; 
+            $images = array_diff(scandir($image_folder), array('.', '..')); 
+            
+            foreach ($images as $image) {
+                echo "<option value='$image'>$image</option>";
+            }
+            ?>
+        </select><br><br>
+
+        <button type="submit">Save Content</button>
+    </form>
+
+</body>
+</html>
