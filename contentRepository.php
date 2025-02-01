50441f4381b71d47416a9b7375388370ccd9f188
OketaShuki <os64936@ubt-uni.net>
2025-02-01 06:19:38 -0800

    Krijimi i file contentRepository.php ku ky file ben lidhjen me databazen ne menyre qe te merren te gjitha te dhenat dhe te vendosen tek tabela e dashboardcontent.php

---
+++ contentRepository.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'DatabaseConnection.php';
+
+class ContentRepository {
+    private $conn;
+
+    public function __construct() {
+        $db = new DatabaseConnection();
+        $this->conn = $db->startConnection();
+    }
+
+    public function getAllContent() {
+        $sql = "SELECT * FROM content ORDER BY created_at DESC";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
